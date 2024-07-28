91bbc82d25d01741efbb7104c0bf828a31fc1a15
osh <dhtpgus7@gmail.com>
2024-07-28 21:59:26 +0900

    feat: NodeJs 컨테이너의 볼륨, 바인드 마운트 및 폴리싱

--- multi-starting-setup-01/backend/app.js
+++ multi-starting-setup-01/backend/app.js
@@ -87,17 +87,18 @@ mongoose.connect(
   // 'mongodb://localhost:27017/course-goals',
   // "mongodb://host.docker.internal:27017/course-goals",
   // "mongodb://mongodb:27017/course-goals",
-  "mongodb://max:secret@mongoDB:27017/course-goals?authSource=admin",
+  // "mongodb://max:secret@mongoDB:27017/course-goals?authSource=admin",
+  `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongoDB:27017/course-goals?authSource=admin`,
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true, 
   },
   (err) => {
     if (err) {
       console.error("FAILED TO CONNECT TO MONGODB");
       console.error(err);
     } else {
-      console.log("CONNECTED TO MONGODB");
+      console.log("CONNECTED TO MONGODB!!");
       app.listen(80);
     }
   }
